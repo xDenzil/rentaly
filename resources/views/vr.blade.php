32bce23a5f4f0a2da5f679ad41a96ff5859f476c
Denzil Williams <denzzilwilliams@gmail.com>
2020-07-21 17:04:09 -0500

    vr view changes

--- resources/views/vr.blade.php
+++ resources/views/vr.blade.php
@@ -5,7 +5,7 @@
 @section('body-class','bg-light')
 
 @section('content')
-<div style="height:92vh; position: relative">
+<div style="height:85vh; position: relative">
     <div style="height:100%">
         <iframe name="Framename" src="https://rentaly-vrroomdemo.herokuapp.com/" frameborder="0" scrolling="auto"
             class="frame-area" style="height:100%; width:100%;">
@@ -13,7 +13,7 @@
 
         <div id="status-box" class="position-absolute bg-danger rounded text-white p-3"
             style="top:5%; left: 5%; cursor: pointer;" onclick="location.href='../feed/{{$prop_id}}'">
-            <h4>Exit VR<i class="fa fa-times ml-3"></i></h4>
+            <p class="m-0 p-0">Exit VR<i class="fa fa-times ml-3"></i></p>
         </div>
     </div>
 </div>
